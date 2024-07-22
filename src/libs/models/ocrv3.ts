bf21487659e6c84505b176e10816e266f36780c7
feixue <feixue@esign.cn>
2024-07-22 09:58:24 +0800

    init

--- src/libs/models/ocrv3.ts
+++ src/libs/models/ocrv3.ts
@@ -2,11 +2,11 @@ import {OcrModel} from "@/libs/models/type.ts";
 
 export default {
     detModel: {
-        modelPath: "models/ch_PP-OCRv3_det_infer.onnx"
+        modelPath: "https://cdn.jsdelivr.net/gh/utools-blowsnow/ocr-web@models/ch_PP-OCRv3_det_infer.onnx"
     },
     recModel: {
-        modelPath: "models/ch_PP-OCRv3_rec_infer.onnx"
+        modelPath: "https://cdn.jsdelivr.net/gh/utools-blowsnow/ocr-web@models/ch_PP-OCRv3_rec_infer.onnx"
     },
-    charesPath: "models/ppocr_keys_v1.txt"
+    charesPath: "https://cdn.jsdelivr.net/gh/utools-blowsnow/ocr-web@models/ppocr_keys_v1.txt"
 
 } as OcrModel
